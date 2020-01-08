89cfd1abbe34acdd9aa1dc65072413e01533aec9
hahaha98389 <359075446@qq.com>
2020-01-08 16:22:45 +0800

    service

--- src/main/java/com/example/demoserver/controller/PersonController.java
+++ src/main/java/com/example/demoserver/controller/PersonController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequestMapping("/Person")
 @RestController
 public class PersonController {
@@ -15,7 +17,7 @@ public class PersonController {
     PersonInterface personInterface;
 
     @GetMapping("/getPerson")
-    public Person getPerson() {
+    public List<Person> getPerson() {
        return personInterface.getPerson();
     }
 }
