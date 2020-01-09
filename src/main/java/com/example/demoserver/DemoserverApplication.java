7d399e9c10d73e3d433bcb1c1751535c3f3b5dac
hahaha98389 <359075446@qq.com>
2020-01-09 10:38:31 +0800

    service

--- src/main/java/com/example/demoserver/DemoserverApplication.java
+++ src/main/java/com/example/demoserver/DemoserverApplication.java
@@ -3,8 +3,11 @@ package com.example.demoserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan("com.example.demoserver.dao.interfaces")
+//SpringbootApplication 禁止加载自身对DataSource的扫描
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class DemoserverApplication {
 
