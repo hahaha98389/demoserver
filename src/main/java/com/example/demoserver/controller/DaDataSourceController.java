5e8c940cd6b31bb20c65c521cb44f11a4f2471b5
hahaha98389 <359075446@qq.com>
2020-01-10 18:45:16 +0800

    配置多数据源

--- src/main/java/com/example/demoserver/controller/DaDataSourceController.java
+++ src/main/java/com/example/demoserver/controller/DaDataSourceController.java
@@ -6,6 +6,8 @@ import com.example.demoserver.dao.po.da2.Money;
 import com.example.demoserver.dao.po.da2.MoneyExample;
 import com.example.demoserver.dao.po.da3.User;
 import com.example.demoserver.dao.po.da3.UserExample;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(description = "用户接口 ")
 @RequestMapping("/test")
 @RestController
 public class DaDataSourceController {
@@ -23,6 +26,7 @@ public class DaDataSourceController {
     @Autowired
     UserMapper userMapper;
 
+    @ApiOperation(value = "登录")
     @RequestMapping("/getMoney")
     public List<Money> getMoney(){
         MoneyExample moneyExample = new MoneyExample();
