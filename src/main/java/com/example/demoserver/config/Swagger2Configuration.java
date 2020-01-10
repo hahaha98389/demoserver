c8545dc90126a707d5357563fabe7de68c7f6563
hahaha98389 <359075446@qq.com>
2020-01-10 18:46:08 +0800

    配置多数据源

---
+++ src/main/java/com/example/demoserver/config/Swagger2Configuration.java
@@ -0,0 +1,71 @@
+package com.example.demoserver.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration implements WebMvcConfigurer {
+
+    @Bean
+    public Docket createRestApi() {
+        // 参数配置
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("Authorization").description("user token")
+                .modelRef(new ModelRef("string")).parameterType("header") // header表示参数放在请求头
+                .required(false); //header中的Authorization参数非必填，传空也可以
+        pars.add(tokenPar.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true) // 动态决定是否启动swagger
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))//这个代码说明的我们扫描的哪些接口，我这行意思是扫描带@Api注解的接口类
+                //也可以这样，说明扫描哪个包下的类
+                // .apis(RequestHandlerSelectors.basePackage("com.xxx.xxx.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars);// 参数配置
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("xxx接口文档")
+                .description("xxx相关接口的文档")
+                .termsOfServiceUrl("http://www.xxx.com")
+                .version("1.0")
+                .build();
+    }
+
+    /**
+     * 静态资源配置
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+
+    }
+
+}
